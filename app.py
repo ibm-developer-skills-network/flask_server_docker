2162a4733fef34365ec15e85d9a6dc3a56ba5bd5
lavskillup <lavanyas@skillup.tech>
2023-09-14 18:16:37 +1000

    Added code for returning values

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from os import environ 
+
+username = environ.get('username',"the Learner")
+
+app = Flask("My Flask Application")
+
+@app.route("/")
+def hello():
+    return "<h1>This is {}'s first session!</h1>".format(username)
+
+
+if __name__=="__main__":
+    app.run(debug=True) 
+    # When no port is specified, starts at default port 5000
