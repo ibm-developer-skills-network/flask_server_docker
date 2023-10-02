82f886e797b606ff1a961031d97cf2de4ab0ab56
lavskillup <73978832+lavskillup@users.noreply.github.com>
2023-10-03 06:56:56 +1100

    Update app.py

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ app = Flask("My Flask Application")
 
 @app.route("/")
 def hello():
-    return "<h1>This is {}'s first session!</h1>".format(username)
-
-
+    return "<h1>This is the {}'s first cloud application</h1>".format(username)
+    
 if __name__=="__main__":
     app.run(debug=True) 
     # When no port is specified, starts at default port 5000
